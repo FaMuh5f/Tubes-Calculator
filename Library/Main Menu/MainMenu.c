75c640232c5ec14605815d41a40a78c2f7e87cbd
NauvalOzora <77134160+NauvalOzora@users.noreply.github.com>
2021-07-25 16:02:46 +0700

    Add files via upload

---
+++ Library/Main Menu/MainMenu.c
@@ -0,0 +1,30 @@
+#include "MainMenu.h"
+int MainMenu(){
+		puts("\n=`=`=`=`=`=`=`=`=` KALULATOR `=`=`=`=`=`=`=`=`=");
+		puts("\n*** MAIN MENU ***\n");
+		puts("1. Kalkulator");
+		puts("2. Zakat");
+		puts("3. About");
+		puts("4. Exit");
+		puts("Enter according to the number choice : ");
+		switch(getch()) {	
+		    case '1' :
+		    	Calculator();
+				break;
+			case '2' : 
+				Zakat();
+				break;
+				break;
+			case '3' :
+				Identity();
+				break;
+			case '4' :
+				system("cls");
+				return 0;
+			default :
+				puts("\nThe Number is unvailable, try again!");
+			}
+		printf("\n");
+		system("PAUSE");
+		system("cls");
+}
---
+++ Library/Main Menu/MainMenu.h
@@ -0,0 +1,12 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+#include "../About/About.h"
+#include "../Zakat/Zakat.h"
+#include "../Calculator/Calculator.h"
+int MainMenu();
+/* Menampilkan menu utama aplikasi ini.			*/
+/* I.S   : Sembarang.							*/
+/* F.S   : Main Menu ditampilkan.		*/
+
+#endif
